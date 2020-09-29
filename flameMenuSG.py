32aa6c7365c0e68fdc3f5f061ee85ce05fc3c582
Andriy Toloshny <50490408+talosh@users.noreply.github.com>
2020-09-29 23:18:12 +0100

    version v0.0.18

--- flameMenuSG.py
+++ flameMenuSG.py
@@ -65,7 +65,7 @@ default_flame_export_presets = {
     'Thumbnail': {'PresetVisibility': 3, 'PresetType': 0, 'PresetFile': 'Generate Thumbnail.xml'}
 }
 
-__version__ = 'v0.0.18-rc.1'
+__version__ = 'v0.0.18'
 
 
 class flameAppFramework(object):
